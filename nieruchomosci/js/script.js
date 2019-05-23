729a9e36329e3c895d6b50ac5bdc37f8203f8d87
Paweł Kobylarski <cobzarocks@gmail.com>
2019-05-23 19:11:54 +0200

    h

--- nieruchomosci/js/script.js
+++ nieruchomosci/js/script.js
@@ -6,3 +6,17 @@ const siteNavigation = document.getElementById("site-navigation");
 buttonMenuToggle.addEventListener("click", function() {
   siteNavigation.classList.toggle("active");
 });
+
+/* AUTO PADDING FOR FOOTER */
+
+const siteBody = document.getElementById("site-body");
+const siteFooter = document.getElementById("site-footer");
+
+// window.addEventListener("load", function() {
+//   bodyPadding(siteFooter, siteBody);
+// });
+
+function bodyPadding(element, elementNeedsPadding) {
+  elementNeedsPadding.style = "padding-bottom: " + element.offsetHeight + "px;";
+  console.log(element.offsetHeight);
+}
